bef9c294e8b063eb0f5cc36160316c54f91be48e
Brian Stanley <Brian Stanley>
2017-01-30 16:43:07 -0500

    Postman and Postgres are talking

--- server/app.js
+++ server/app.js
@@ -27,19 +27,22 @@ var User = sequelize.define('user', {
 //mathces the model we defined
 //Doesn't drop the db
 User.sync();
-//User.sync({ force:true });  //drops the table completely
+
+/******DANGER: THIS WILL DROP THE USER TABLE***
+//User.sync({ force: true });  //drops the table completely
+*****/
 
 app.use(bodyParser.json());
 
-app.post('/api/user', function(req, res) {
+app.post('/api/user', function(req, res){
 	//when we post to api user, it will want a user object in the body
 	var username = req.body.user.username;
-	var pass = req.body.user.username;
+	var pass = req.body.user.password;
 	//Need to create a user object and use sequelize to put that user into
 	//our database.
-	user.create({
+	User.create({
 		username: username,
-		passwordhash: ""
+		passwordhash: ''
 	}).then(
 			//Sequelize is going to return the object it created from db.
 		function createSuccess(user){
