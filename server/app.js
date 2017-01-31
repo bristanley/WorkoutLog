949fa0dc3fcdc99ec39a5cd651d29ced638d4cae
Brian Stanley <Brian Stanley>
2017-01-31 13:36:03 -0500

    login

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ User.sync();//User.sync({ force: true });  //WARNING: This will DROP the table!
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
 app.use('/api/user', require('./routes/user'));
+app.use('/api/login', require('./routes/session'));
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,9 @@
 var router = require('express').Router();
 var sequelize = require('../db.js');
 var User = sequelize.import('../models/user');
+var bcrypt =  require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
 
 
 router.post('/', function(req, res){
@@ -11,14 +14,17 @@ router.post('/', function(req, res){
 	//our database.
 	User.create({
 		username: username,
-		passwordhash: ''
+		passwordhash: bcrypt.hashSync(pass, 10)
 	}).then(
 			//Sequelize is going to return the object it created from db.
 		function createSuccess(user){
 
+			var token = jwt.sign({id: user.id}, 'i_am_secret', {expiresIn: 60*60*24});
+
 			res.json({
 				user: user,
-				message: 'create'
+				message: 'created',
+				sessionToken: token
 			});
 		},
 		function createError(err){
